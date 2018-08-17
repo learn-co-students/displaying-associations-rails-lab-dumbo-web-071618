516189ab0ff980903b33a35542b66c3d99905017
Jason Pitts <juniorjr083@gmail.com>
2018-08-17 14:45:05 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-end
+  
+  def artist_name
+    self.artist.name
+  end
+
+  def full_name
+    self.artist_name+' - '+self.title
+  end
+
+ end 
